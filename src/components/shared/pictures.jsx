576402806aacdf0f7028112fe40f6af01fa32f59
Bezerra <Lucas.Bezerra@vistajet.com>
2019-05-01 18:30:35 -0300

    upload integration between backend and ui

--- src/components/shared/pictures.jsx
+++ src/components/shared/pictures.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { Upload, Button, Icon } from 'antd';
+import { Upload, Button, Icon, message } from 'antd';
+import { request } from 'graphql-request';
 import 'antd/dist/antd.css';
 import '../../template/style.css';
+
+const mutation = `mutation sendPictureMutation($input: [PictureInput]) {
+  sendPicture(input: $input) {
+    message
+    statusCode
+  }
+}`;
+
+const endpoint = 'http://localhost:8083/graphql';
 
 class Pictures extends Component {
   state = {
     fileList: [],
-    uploading: false
+    uploading: false,
+    disabled: true
   }
 
   handleBefore = (file) => {
     const { fileList } = this.state;
     const copy = [...fileList];
     copy.push(file);
-    this.setState({ fileList: copy });
+    this.setState({ fileList: copy, disabled: false });
     return false;
   }
 
@@ -40,19 +51,52 @@ class Pictures extends Component {
     return Promise.all(baseList);
   }
 
+  mapMutation = (list) => {
+    return list.map((item) => {
+      return {
+        picture: item
+      };
+    });
+  }
+
+  handleRemove = (file) => {
+    this.setState((state) => {
+      const index = state.fileList.indexOf(file);
+      const newFileList = state.fileList.slice();
+      newFileList.splice(index, 1);
+      return {
+        fileList: newFileList
+      };
+    });
+  }
+
   handleUpload = async () => {
     const { fileList } = this.state;
     this.setState({ uploading: true });
     const list = await this.getBaseList(fileList);
-    console.log('list base64 ', list);
+    const listInput = this.mapMutation(list);
+    const variables = { input: listInput };
+    console.log('variables ', variables);
+    request(endpoint, mutation, variables)
+    .then(data => {
+      const { sendPicture } = data;
+      const { statusCode } = sendPicture;
+      if (statusCode === 200) {
+        this.setState({ uploading: false, disabled: true });
+        message.success('ok');
+      }
+      console.log('sendPicture ', sendPicture);
+    });
   }
 
   render() {
-      const { fileList, uploading } = this.state;
+      const { fileList, uploading, disabled } = this.state;
+      console.log('fileList ', fileList, 'upload ', uploading);
       return(
             <div className='uploadContainer'>
               <Upload
                 beforeUpload={this.handleBefore}
+                onRemove={this.handleRemove}
                 listType='picture'
                 defaultFileList={fileList}>
                 <Button className='showButton'>
@@ -62,7 +106,7 @@ class Pictures extends Component {
               <Button
                 type="primary"
                 onClick={this.handleUpload}
-                disabled={fileList.length === 0}
+                disabled={disabled}
                 loading={uploading}
                 style={{ marginTop: 16 }}
               >
